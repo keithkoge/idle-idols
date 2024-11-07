d0409b363a62a10fe629d853e36bce059cb866ef
keithkoge <56942327+keithkoge@users.noreply.github.com>
2024-11-06 16:02:02 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const idolprops = ["name", "age", "grade", "pref", "stress", "fatigue", "vocal", "dance", "acting", "smarts", "visuals", "favor", "fans"];
+const idol = {
+  name: "",
+  age: 0,
+  grade: "",
+  pref: "",
+  stress: 0,
+  fatigue: 0,
+  vocal: 0,
+  dance: 0,
+  acting: 0,
+  smarts: 0,
+  visuals: 0,
+  favor: 0,
+  fans: 0,
+};
+
